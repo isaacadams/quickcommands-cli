7e2ac0123e63a0b99eebfa6f82722cbfe38a6f2e
Isaac Adams <iadams@emoneyadvisor.com>
2018-11-19 09:47:22 -0500

    removing unnessecary init.py files and cleaning up code no longer needed in cli.py

--- source/python/cli.py
+++ source/python/cli.py
@@ -23,11 +23,7 @@ parser.add_argument('-d', '--directory', help='add the full path to the director
 parser.add_argument('-u', '--users', help='add the names of the users you would like to generate the report on, example: iadams, "Isaac Adams"', nargs='+', required=True)
 parser.add_argument('-t', '--time', help='the timeframe that you want to report on example: "1 week ago"', required=True)
 args = parser.parse_args()
-#print(args.users)
 
-bases = ['C:\\source', 'C:\\source\\LBS']
-users = ['Isaac Adams', 'iadams']
-print(args.directory)
 GitReport(args.directory, args.users, args.time).report()
 
 
